28908b72320484e2912455c29500dcf684794659
k-ojo <gideonad4@gmail.com>
2024-09-09 13:28:55 +0000

    modified lnklst.c

--- lnklst.c
+++ lnklst.c
@@ -52,6 +52,48 @@ void _insert(node **head, int x)
     *head = nd;
 }
 
+
+/**
+* _insert- inserts data at the beginning of list
+* @head: head of linked list
+* @x: input data
+* @n: index to insert
+* Return: the new head of the linked list
+*/
+int _insertn(node **head, int x, int n)
+{
+    if (n == 0)
+    {
+        _insert(head, x);
+        return (1);
+    }
+
+    int i = 0;
+    node *hold;
+    node *tmp = (node *)malloc(sizeof(node));
+    
+    tmp->data = x;
+    hold = *head;
+
+    while (hold != NULL)
+    {
+        if (i == n - 1)
+        {
+            tmp->next = hold->next;
+            hold->next = tmp;
+            return (1);
+        }
+        hold = hold->next;
+        i++;
+    }
+    if (n > i)
+    {
+        printf("Error: %d is not a valid index\n", n);
+    }
+    free(tmp);
+    return (0);
+}
+
 /**
 * _printlist- prints linked list
 * @head: head of linked list
@@ -65,11 +107,26 @@ void _printlist(node *head)
         printf("%i->", head->data);
         head = head->next;
     }
-    printf("%i->NULL", head->data);
-    
+    printf("%i->NULL\n", head->data);
+     
 }
 
-/**
+node *_reversels(node *head)
+{
+    node *prev = NULL;
+    node *cur = head;
+    node *nxt;
+
+    while(cur != NULL)
+    {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    return prev;
+}
+/** 
 * _freelist: frees the list from allocated memory
 * @head- head of linked list
 * Return: Nothing
@@ -77,6 +134,10 @@ void _printlist(node *head)
 void _freelist(node *head)
 {
     node *tmp;
+    if (!head)
+    {
+        return;
+    }
     while (head->next != NULL)
     {
         tmp = head;
@@ -86,17 +147,53 @@ void _freelist(node *head)
     free(head);
 }
 
+int _deleten(node **hptr, int n)
+{
+    int i = 0;
+    node *tmp;
+    node *head = *hptr;
+
+    if (n == 0)
+    {
+        *hptr = head->next;
+        free(head);
+        return (1);
+    }
+
+    while(head->next != NULL)
+    {
+        if (i == n - 1)
+        {
+            tmp = head->next;
+            head->next = tmp->next;
+            free(tmp);
+            return (1);
+        }
+        i++;
+        head = head->next;
+    }
+    printf("Invalid index\n");
+    return (0);
+
+}
+
 //test
 int main(void)
 {
     // Write C code here
     node *head = NULL;
+    node *h2 = NULL;
     _insert(&head, 25);
     _insert(&head, 80);
     _append(head, 60);
-    _append(head, 80);
+    _append(head, 83);
     _append(head, 58);
+    _insertn(&head, 85, 5);
+    _deleten(&head, -1);
+    h2 = _reversels(head);
     _printlist(head);
+    _printlist(h2);
     _freelist(head);
+    _freelist(h2);
     return 0;
 }
