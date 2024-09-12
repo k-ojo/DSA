0bb31ec7a26c5c5ab321b7d99cdc806dcc0b7094
k-ojo <gideonad4@gmail.com>
2024-09-12 11:14:16 +0000

    modified dblnklst.c

--- 1-ds_dblnklst/dblnklst.c
+++ 1-ds_dblnklst/dblnklst.c
@@ -2,10 +2,33 @@
 
 void _dinsert(dnode **head, int x)
 {
+    
     dnode *tmp = (dnode *)malloc(sizeof(dnode));
     tmp->data = x;
     tmp->next = *head;
     tmp->prev = NULL;
+    if (*head == NULL)
+    {
+        *head = tmp;
+        return;
+    }
     (*head)->prev = tmp;
     *head = tmp;
+}
+
+void _dprintls(dnode *head)
+{
+    if (head == NULL)
+    {
+        printf("NULL\n");
+        return;
+    }
+    printf("NULL<---");
+    while(head->next != NULL)
+    {
+        printf("%i<--->", head->data);
+        head = head->next;
+    }
+    printf("%i-->NULL\n", head->data);
+    
 }
--- 1-ds_dblnklst/test.c
+++ 1-ds_dblnklst/test.c
@@ -1,5 +1,16 @@
+#include "dblnklst.h"
+
 int main(void)
 {
-    printf("Hello world!\n");
+    dnode *head = NULL;
+
+    _dinsert(&head, 45);
+    _dinsert(&head, 56);
+    _dinsert(&head, 9);
+    _dinsert(&head, 78);
+
+
+    _dprintls(head);
+
     return (0);
 }
