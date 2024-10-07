d050c4b35d951864ad7b24ae0b8578cf915a9fad
root <root@DESKTOP-L0484C5>
2024-10-07 11:02:23 +0000

    bugs

--- 6-Hash/hash.h
+++ 6-Hash/hash.h
@@ -3,5 +3,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../4-ds_trees/src/tree.h"
+#include <string.h>
+
+typedef int tsize;
+typedef int ksize;
+
+//typedef struct _hashMap map;
+
+typedef enum _type
+{
+    INT,
+    STR
+}type;
+
+typedef struct _hashMap
+{
+    type Type;
+    tsize size;
+    bnode **arr;
+    int (*H)(void *, tsize);    
+}map;
+
+//hash.c
+ksize Hint(void *id, tsize);
+ksize Hstr(void *id, tsize);
+map *Hinit(tsize s, type);
+int _mapit(map *m, void *data, void *id);
+int mapit(map *m, int, int id);
+int prdata(map *m, int);
+
 
 #endif
--- 6-Hash/test.c
+++ 6-Hash/test.c
@@ -1,6 +1,27 @@
 #include "hash.h"
 
+typedef struct _data
+{
+    char *name;
+    int age;
+    int id;
+}data;
+
 int main (void)
 {
+    map *m = Hinit(10, INT);
+    mapit(m, 7, 0);
+
+    mapit(m, 72, 1);
+    mapit(m, 44, 2);
+    //binotr(m->arr[2]);
+
+    mapit(m, 45, 3);
+    //printf("\t%\n",  *(int *)m->arr[0]->data);
+    //prdata(m, 0);
+
+
+
+
     return (1);
 }
